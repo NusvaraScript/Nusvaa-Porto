067cacc29b847e24202e8f2ec4fb37d7d660942f
Nusvara <yusufusman11111111@gmail.com>
2026-02-18 09:10:52 +0700

    Navbar progress

--- resources/views/components/page-comps/navbar.blade.php
+++ resources/views/components/page-comps/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex items-center">
-                <a href="/home" class="text-2xl font-bold text-blue-600">
+                <a href="/home" class="text-2xl font-bold text-blue-800">
                     Nusvaa<span class="text-gray-800">Porto.</span>
                 </a>
             </div>
@@ -11,11 +11,9 @@
                 <a href="/about" class="text-gray-600 hover:text-blue-600 transition">Tentang</a>
                 <a href="/projects" class="text-gray-600 hover:text-blue-600 transition">Projek Saya</a>
                 <a href="/blog" class="text-gray-600 hover:text-blue-600 transition">Blog</a>
-                <a href="/contact" class="text-gray-600 hover:text-blue-600">Kontak Saya</a>
+                <a href="/contact"
+                    class="px-4 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition">Kontak Saya</a>
             </div>
-        </div>
-        <div>
-
         </div>
     </div>
 </nav>
