309f842d8bf931110b17c2b624e945fdf6b6eb6a
Nusvara <yusufusman11111111@gmail.com>
2026-03-15 12:25:22 +0700

    first commit for the about page

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -1,2 +1,9 @@
 @extends('layout.user')
 @section('title', 'Tentang')
+@section('content')
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <x-hero title="Who am i?">
+        <p>I dont know, i guess i am a web developer.</p>
+    </x-hero>
+</div>
+@endsection
